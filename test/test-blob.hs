d2fde58818192988d40f76b1d578dad5ff3a2b44
Anshu Avinash <anshu.avinash35@gmail.com>
2015-05-06 20:51:03 +0530

    Add tests for GC

--- test/test-blob.hs
+++ test/test-blob.hs
@@ -1,4 +1,5 @@
 import           Data.Blob
+import           Data.Blob.GC
 import           Data.ByteString.Char8   (pack)
 import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
@@ -13,14 +14,43 @@ writeStringToBlob s = createBlob testDir >>=
   (\wc -> writePartial wc (Blob (pack s))) >>=
   finalizeWrite
 
+-- | Read given length from a location
+readFromBlob :: Int -> Location -> IO Blob
+readFromBlob size loc = do
+  rc <- initRead loc
+  bs <- readPartial rc size
+  finalizeRead rc
+  return bs
+
 -- | Should be able to read from blob just written to
 propWriteAndRead :: String -> Property
 propWriteAndRead s = monadicIO $ do
   loc <- run $ writeStringToBlob s
+  bs <- run $ readFromBlob (length s) loc
+  assert (Blob (pack s) == bs)
+
+-- | Should be able to read blobs during GC
+propReadDuringGC :: String -> Property
+propReadDuringGC s = monadicIO $ do
+  loc <- run $ writeStringToBlob s
   rc <- run $ initRead loc
+  run $ startGC testDir
+  run $ endGC testDir
   bs <- run $ readPartial rc (length s)
   run $ finalizeRead rc
   assert (Blob (pack s) == bs)
 
+-- | Should be able to create blobs during GC
+propCreateDuringGC :: String -> Property
+propCreateDuringGC s = monadicIO $ do
+  run $ startGC testDir
+  loc <- run $ writeStringToBlob s
+  run $ endGC testDir
+  bs <- run $ readFromBlob (length s) loc
+  assert (Blob (pack s) == bs)
+
 main :: IO ()
-main = quickCheckWith stdArgs { maxSuccess = 50 } propWriteAndRead
+main = do
+  quickCheckWith stdArgs { maxSuccess = 50 } propWriteAndRead
+  quickCheckWith stdArgs { maxSuccess = 50 } propReadDuringGC
+  quickCheckWith stdArgs { maxSuccess = 50 } propCreateDuringGC
